79f9022c9a6e356e8f34eb5f844bcdd3def0bd61
con-stella <infatuate85@gmail.com>
2024-10-31 11:27:48 +0900

    Update App_VideoPlay.py

--- App_VideoPlay.py
+++ App_VideoPlay.py
@@ -9,6 +9,8 @@ st.set_page_config(layout="wide")
 st.title("비디오 사물 검출 앱")
 
 
+uploaded_file = st.file_uploader("비디오 파일을 업로드하세요", type=["mp4", "mov", "avi"])
+
 # 모델 파일 업로드
 model_file = st.file_uploader("모델 파일을 업로드하세요", type=["pt"])
 if model_file:
